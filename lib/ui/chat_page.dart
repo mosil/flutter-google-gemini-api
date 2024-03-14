964fa6b6b342fcaed7f7223b3fbb31d3ea28228f
Mosil <mosil.biz@gmail.com>
2024-03-14 17:53:57 +0800

    add try catch

--- lib/ui/chat_page.dart
+++ lib/ui/chat_page.dart
@@ -103,19 +103,28 @@ class _ChatPageState extends State<ChatPage> {
       model: aiModel,
     );
     final List<Content> contents = [Content.text(text)];
-    final GenerateContentResponse response = await model.generateContent(contents);
-    // show correct text on chat list, when success.
-    setState(() {
-      MessageModel feedback = MessageModel(
-        type: MessageType.feedback,
-        text: response.text ?? "",
-      );
-      messages.add(feedback);
-      _scrollDown();
-
-      _inputController.text = "";
-      isLoading = false;
-    });
+    try {
+      final GenerateContentResponse response = await model.generateContent(contents);
+      // Success: show correct text to message list in chat.
+      setState(() {
+        MessageModel feedback = MessageModel(
+          type: MessageType.feedback,
+          text: response.text ?? "",
+        );
+        messages.add(feedback);
+        _scrollDown();
+      });
+    } on GenerativeAIException catch (e) {
+      // Error: show dialog.
+      setState(() {
+        _showError(context: context, error: e.message);
+      });
+    } finally {
+      setState(() {
+        _inputController.text = "";
+        isLoading = false;
+      });
+    }
   }
 
   void _scrollDown() {
